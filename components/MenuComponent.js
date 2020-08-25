392e36fd2083bc6d46bf53ad61b89c20e94ca658
nada <nada.baklouti@esprit.tn>
2020-08-25 14:09:15 +0100

    Activity Indicator

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -4,15 +4,14 @@ import { ListItem } from 'react-native-elements';
 import { Tile } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
+
 
 const mapStateToProps = state => {
     return {
         dishes: state.dishes
     }
 }
-
-
-
 
 class Menu extends Component {
 
@@ -38,13 +37,27 @@ class Menu extends Component {
 
         const { navigate } = this.props.navigation;
 
-        return (
-            <FlatList
-                data={this.props.dishes.dishes}
-                renderItem={renderMenuItem}
-                keyExtractor={item => item.id.toString()}
-            />
-        );
+        if (this.props.dishes.isLoading) {
+            return (
+                <Loading />
+            );
+        }
+        else if (this.props.dishes.errMess) {
+            return (
+                <View>
+                    <Text>{props.dishes.errMess}</Text>
+                </View>
+            );
+        }
+        else {
+            return (
+                <FlatList
+                    data={this.props.dishes.dishes}
+                    renderItem={renderMenuItem}
+                    keyExtractor={item => item.id.toString()}
+                />
+            );
+        }
     }
 }
 
