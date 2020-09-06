b2183ae2202facf23282e9ca9a4f88f0015d32df
nada <nada.baklouti@esprit.tn>
2020-09-06 22:38:17 +0100

    Sending Email

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import { Text, ScrollView, View } from 'react-native';
-import { Card } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
+import { Card, Button, Icon } from 'react-native-elements';
+import * as MailComposer from 'expo-mail-composer';
 
 
 class Contact extends Component {
+    sendMail() {
+        MailComposer.composeAsync({
+            recipients: ['nadbak95@gmail.com'],
+            subject: 'Enquiry',
+            body: 'To whom it may concern:'
+        })
+    }
 
     render() {
 
@@ -20,6 +28,13 @@ class Contact extends Component {
                             Tel: +852 1234 5678{"\n"}
                             Fax: +852 8765 4321{"\n"}
                             Email:confusion@food.net</Text>
+
+                        <Button
+                            title="Send Email"
+                            buttonStyle={{ backgroundColor: "#512DA8" }}
+                            icon={<Icon name='envelope-o' type='font-awesome' color='white' />}
+                            onPress={this.sendMail}
+                        />
                     </Card>
                 </Animatable.View>
             </ScrollView>
